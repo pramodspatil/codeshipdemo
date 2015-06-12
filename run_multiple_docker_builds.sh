eb51db6472e918a8cfab9fc2abd48e0ea4668b95
Florian Motlik <flomotlik@gmail.com>
2015-06-12 17:11:25 -0400

    Prepare benchmark scripts for parallelisation

--- run_multiple_docker_builds.sh
+++ run_multiple_docker_builds.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 
+server=$1
+
 for times in {1..10}
 do
   for build in serial #optimized parallel
   do
     cp codeship-steps.$build.yml codeship-steps.yml
-    for server in t2.medium c4.large c4xlarge
-    do
-      echo "Activating Docker Machine: $build.$server.$times"
-      echo "-----------------"
-      docker-machine active $server
-      docker-machine ls
-      eval "$(docker-machine env $server)"
-      env | grep DOCKER
-      mkdir -p tmp/logs
-      time jet steps &> tmp/logs/$build.$server.$times.log
-      echo "-----------------"
-    done
+    echo "Activating Docker Machine: $build.$server.$times"
+    echo "-----------------"
+    date
+    docker-machine active $server
+    docker-machine ls
+    eval "$(docker-machine env $server)"
+    env | grep DOCKER
+    mkdir -p tmp/logs
+    #time jet steps &>
+    echo $server > tmp/logs/$build.$server.$times.log
+    echo "-----------------"
   done
 done
