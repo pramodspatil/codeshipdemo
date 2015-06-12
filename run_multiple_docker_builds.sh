f9614106846a81d46e3a1338d04689acd2e1c48c
Florian Motlik <flomotlik@gmail.com>
2015-06-12 13:38:48 -0400

    Run Benchmark 10 times

--- run_multiple_docker_builds.sh
+++ run_multiple_docker_builds.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 
-for build in serial #optimized parallel
+for times in {1..10}
 do
-  cp codeship-steps.$build.yml codeship-steps.yml
-  for server in t2.medium c4.large c4xlarge
+  for build in serial #optimized parallel
   do
-    echo "Activating Docker Machine: $server.$build"
-    echo "-----------------"
-    docker-machine active $server
-    docker-machine ls
-    eval "$(docker-machine env $server)"
-    env | grep DOCKER
-    mkdir -p tmp/logs
-    time jet steps &> tmp/logs/$build.$server.log
-    echo "-----------------"
+    cp codeship-steps.$build.yml codeship-steps.yml
+    for server in t2.medium c4.large c4xlarge
+    do
+      echo "Activating Docker Machine: $build.$server.$times"
+      echo "-----------------"
+      docker-machine active $server
+      docker-machine ls
+      eval "$(docker-machine env $server)"
+      env | grep DOCKER
+      mkdir -p tmp/logs
+      time jet steps &> tmp/logs/$build.$server.$times.log
+      echo "-----------------"
+    done
   done
 done
