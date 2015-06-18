373dc8de9f67d3fba8fb08272086d9d6477c508a
Florian Motlik <flomotlik@gmail.com>
2015-06-18 14:48:50 -0400

    Create docker-machine if it doesn't exist

--- run_multiple_docker_builds.sh
+++ run_multiple_docker_builds.sh
@@ -4,11 +4,11 @@ server=$1
 
 for times in {1..5}
 do
-  for build in parallel serial #optimized
+  for build in serial parallel optimized
   do
     log_folder_name="tmp/logs/$server/$build"
     log_file_name="$build.$times"
-    log_path="$log_folder_name/$log_file_name"
+    log_path="$log_folder_name/$log_file_name.log"
 
     rm -fr $log_folder_name
     mkdir -p $log_folder_name
@@ -21,9 +21,13 @@ do
     docker-machine ls
     eval "$(docker-machine env $server)"
     echo "Docker Cleanup"
-    bash -lc "docker_cleanup" &> $log_path.cleanup.log
+    bash -lc "docker_cleanup" &> $log_path.cleanup
     env | grep DOCKER
-    time jet steps &> $log_path.log
+    time jet steps &> $log_path
+    jet_exit_status=$?
+    echo "EXIT_STATUS: $jet_exit_status"
+    cpu_time=`grep -r COMMAND_RESULTS $log_path | grep -o "\d*\.\d*$" | paste -sd+ - | bc`
+    echo "CPU TIME: $cpu_time"
     echo "-----------------"
   done
 done
