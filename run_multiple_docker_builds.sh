2e3b329485300e5f60f5e537bbbf22099189b526
Florian Motlik <flomotlik@gmail.com>
2015-06-21 08:43:43 -0400

    Add machine and instance cleanup

--- run_multiple_docker_builds.sh
+++ run_multiple_docker_builds.sh
@@ -19,9 +19,6 @@ do
     date
     docker-machine ls
     eval "$(docker-machine env $server)"
-    echo "--- DOCKER CLEANUP ---"
-    docker-machine ssh $server "sudo docker run -v /var/run/docker.sock:/var/run/docker.sock -v /var/lib/docker:/var/lib/docker --rm martin/docker-cleanup-volumes" &> $log_path.cleanup
-    docker-machine ssh $server "df -h"
 
     echo "--- DOCKER ENVIRONMENT ---"
     env | grep DOCKER
@@ -32,6 +29,35 @@ do
     echo "EXIT_STATUS: $jet_exit_status"
     cpu_time=`grep -r COMMAND_RESULTS $log_path | grep -Eo "[0-9]+.[0-9]+" | paste -sd+ - | bc`
     echo "CPU_TIME: $cpu_time"
+
+    echo "--- DOCKER CLEANUP ---"
+
+    # Find all containers that have exited
+    containers=`docker ps -a -q | xargs`
+    if [[ $containers ]]; then
+      # Remove exited containers
+      docker stop $containers
+      docker rm $containers
+    else
+      echo "No containers to remove"
+    fi
+
+    # Find all images that are not tagged
+    images=`docker images -a -q -f dangling=true | xargs`
+    if [[ $images ]]; then
+      # Remove untagged images
+      docker rmi $images
+    else
+      echo "No images to remove"
+    fi
+
+    docker-machine ssh $server "sudo docker run -v /var/run/docker.sock:/var/run/docker.sock -v /var/lib/docker:/var/lib/docker --rm martin/docker-cleanup-volumes" &> $log_path.cleanup
+    docker-machine ssh $server "df -h"
+
+    # List containers and images that remain
+    echo "LEFT OVER CONTAINERS"
+    docker ps -a
+    docker images -a
     echo "-----------------"
   done
 done
